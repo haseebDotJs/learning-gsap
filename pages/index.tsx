7c3205f895fda5f4b7270a01d8a07f1cf9f3d922
haseebDotJs <haseeb.khalid03452800681@gmail.com>
2022-06-21 18:17:58 +0500

    animate boxes on scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,16 @@ import React, { useEffect } from "react";
 
 //
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const Home: NextPage = () => {
-  let tween;
-  if (typeof document !== "undefined") {
-    tween = gsap.fromTo(
-      ".box",
-      {
+  // let tween;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const from = (__className: string) => {
+      return {
+        scrollTrigger: __className,
         rotation: 360,
         duration: 2,
         ease: "elastic",
@@ -17,59 +20,55 @@ const Home: NextPage = () => {
         // immediateRender: true,
         // paused: true,
         opacity: 0,
-      },
-      {
-        opacity: 1,
-        x: 100,
-        scale: 1.5,
-      }
-    );
-  }
-  // useEffect(() => {
-  //   gsap.to(document.querySelectorAll(".box"), {
-  //     rotation: 27,
-  //     x: 100,
-  //     duration: 1,
-  //   });
-  // }, []);
+      };
+    };
+    const to = (__className: string) => {
+      return { scrollTrigger: __className, opacity: 1, x: 400, scale: 2.5 };
+    };
+    gsap.fromTo(".green", from(".green"), to(".green"));
+    gsap.fromTo(".orange", from(".orange"), to(".orange"));
+    gsap.fromTo(".grey", from(".grey"), to(".grey"));
+  }, []);
 
-  const play = () => {
-    tween?.play();
-  };
-  const reverse = () => {
-    tween?.reverse();
-  };
-  const resume = () => {
-    tween?.resume();
-  };
-  const pause = () => {
-    tween?.pause();
-  };
+  // const play = () => {
+  //   tween?.play();
+  // };
+  // const reverse = () => {
+  //   tween?.reverse();
+  // };
+  // const resume = () => {
+  //   tween?.resume();
+  // };
+  // const pause = () => {
+  //   tween?.pause();
+  // };
   return (
     <React.Fragment>
       <div className="home">
-        <div>
+        <div className="box-container">
           <div className="box green"></div>
         </div>
-        <div>
+        <div className="box-container">
           <div className="box orange"></div>
         </div>
-        <div>
+        <div className="box-container">
           <div className="box grey"></div>
         </div>
       </div>
-      <div className="">
-        <button onClick={play}>Play</button>
-      </div>{" "}
-      <div className="">
-        <button onClick={reverse}>Reverse</button>
-      </div>
-      <div className="">
-        <button onClick={resume}>Resume</button>
-      </div>
-      <div className="">
-        <button onClick={pause}>Pause</button>
-      </div>
+      {/* <div>
+        <div className="">
+          <button onClick={play}>Play</button>
+        </div>{" "}
+        <div className="">
+          <button onClick={reverse}>Reverse</button>
+        </div>
+        <div className="">
+          <button onClick={resume}>Resume</button>
+        </div>
+        <div className="">
+          <button onClick={pause}>Pause</button>
+        </div>
+      </div> */}
     </React.Fragment>
   );
 };
